f5b2d8995438bc84d32ad19eb51bd1b4bcc454ef
japaedit <migueligoliveira09@gmail.com>
2026-03-10 18:53:55 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,9 +6,16 @@ authStrategy: new LocalAuth({
 clientId: "diario-bot"
 }),
 puppeteer: {
+headless: true,
 args: [
 "--no-sandbox",
-"--disable-setuid-sandbox"
+"--disable-setuid-sandbox",
+"--disable-dev-shm-usage",
+"--disable-accelerated-2d-canvas",
+"--no-first-run",
+"--no-zygote",
+"--single-process",
+"--disable-gpu"
 ]
 }
 })
